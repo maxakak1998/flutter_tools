7fd2923848d4b2f60a3285ad02265538a6019b10
Kiet <kiet.phi@apps-cyclone.com>
2025-06-16 01:47:19 +0700

    Add missing inject

--- tools/create_core/__brick__/main.dart
+++ tools/create_core/__brick__/main.dart
@@ -1,20 +1,11 @@
 import 'package:{{project_name}}/features/main_app/main_app_inject.dart';
+import 'package:{{project_name}}/features/main_app/main_app_inject.dart';
 
 import 'app_export.dart';
 
 Future<void> main() async {
   injectMainAppModule();
-  // injectInitModule();
-  // injectSignInModule(); // Register SignIn feature DI
-  // injectHomePageModule();
-  // injectSearchMeterModule();
-  // injectMainSetupModule(); // Register MainSetup feature DI
-  // injectDetailMeterModule(); // Register DetailMeter feature DI
-  // injectHelpModule(); // Register Help feature DI
-  // injectMeterReadingModule(); // Register MeterReading feature DI
-  // injectExtraSelectionModule(); // Register ExtraSelection feature DI
-  // injectAddressModule(); // Register Address feature DI
-  // injectAddCustomerModule(); // Register AddCustomer feature DI
+  injectInitModule();
   EasyLocalization.logger.enableBuildModes = [];
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
