27e49bf30dfe67441afae19e5e66d42a0100c97d
Kiet <kiet.phi@apps-cyclone.com>
2025-07-08 10:47:43 +0700

    Update missing imports

--- tools/create_feature/__brick__/{{feature.snakeCase()}}/domain/useCases/get_{{feature.snakeCase()}}_use_case.dart
+++ tools/create_feature/__brick__/{{feature.snakeCase()}}/domain/useCases/get_{{feature.snakeCase()}}_use_case.dart
@@ -1,4 +1,5 @@
 import '../../../../app_export.dart';
+import '../repositories/{{feature.snakeCase()}}_repository_base.dart';
 
 abstract class IGet{{feature.pascalCase()}}UseCase {
 
@@ -6,7 +7,8 @@ abstract class IGet{{feature.pascalCase()}}UseCase {
 }
 
 class Get{{feature.pascalCase()}}UseCase extends IGet{{feature.pascalCase()}}UseCase {
-
+  final I{{feature.pascalCase()}}Repository i{{feature.pascalCase()}}Repository;
+  Get{{feature.pascalCase()}}UseCase(this.i{{feature.pascalCase()}}Repository);
 
 }
 
--- tools/create_feature/__brick__/{{feature.snakeCase()}}/presentation/cubit/{{feature.snakeCase()}}_cubit.dart
+++ tools/create_feature/__brick__/{{feature.snakeCase()}}/presentation/cubit/{{feature.snakeCase()}}_cubit.dart
@@ -1,8 +1,11 @@
 import '../../../../app_export.dart';
 import '{{feature.snakeCase()}}_cubit_state.dart';
+import '../../domain/useCases/get_{{feature.snakeCase()}}_use_case.dart';
 
 
 class {{feature.pascalCase()}}Cubit extends Cubit<BaseCubitState> {
-  {{feature.pascalCase()}}Cubit():super({{feature.pascalCase()}}InitState());
+  final IGet{{feature.pascalCase()}}UseCase iGet{{feature.pascalCase()}}UseCase;
+
+  {{feature.pascalCase()}}Cubit(this.iGet{{feature.pascalCase()}}UseCase) : super({{feature.pascalCase()}}InitState());
 }
 
